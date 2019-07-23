f0bb735f0c60924c36e79271e9ff6411f912e8b0
anacamargo <anaclaudia2501@gmail.com>
2019-07-23 12:42:57 -0300

    [firebase] arquivo de abstração

---
+++ src/firabaseWrapper.js
@@ -0,0 +1,54 @@
+import firebase from '../firebase-config';
+
+class FirebaseAuthWrapper {
+  constructor() {
+    this.auth = firebase.auth();
+  }
+
+  getCurrentUserID() {
+    return this.auth.currentUser.uid;
+  }
+
+  async signInWithEmailAndPassword(email, password) {
+    return await this.auth.signInWithEmailAndPassword(email, password);
+  };
+
+  signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        sessionStorage.clear();
+        window.location = '/';
+      }, function(error) {
+        console.error(error);
+      });
+  }
+}
+
+class FirebaseFirestoreWrapper {
+  constructor() {
+    this.firestore = firebase.firestore();
+  }
+
+  async createUser(obj, id) {
+    await this.firestore.doc(`orders/${id}`).set({
+      name: name,
+      email: email,
+      phone: phone,
+      cpf: cpf,
+      status: status,
+      photoURL: '../images/avatar.png',
+      createdAt: new Date()
+    });
+  }
+
+}
+class FirebaseWrapper {
+  constructor() {
+    this.auth = new FirebaseAuthWrapper();
+    this.firestore = new FirebaseFirestoreWrapper();
+  }
+}
+
+export default new FirebaseWrapper();
