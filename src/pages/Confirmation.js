dc33cdc6de9eca4740fdb2d681ff1010e82dcd88
camebastos <camebastos@gmail.com>
2019-07-24 16:07:33 -0300

    Confirmation[style, html]

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,19 +1,38 @@
-import React from 'react';
 
-class Confirmation extends React.Component {
+import React from 'react';
+import '../pages/Confirmation.css';
+import '../pages/Login.css';
+import '../components/Form.css';
+import firebase from '../firebase-config';
+// import withFirebaseAuth from 'react-with-firebase-auth';
+import { Link } from 'react-router-dom';
+
+
+const firebaseAppAuth = firebase.auth();
+const database = firebase.firestore();
+
+class Confirm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     };
   }
 
+
   render() {
     return (
-      <section>
-        <h1>#Confirmation</h1>
+      <section className='home'>
+
+        <div className='form'>
+      <img className="img" src={require('../images/Logo_Transp.png')} />
+<h1>Seu cadastro foi enviado com sucesso!</h1>
+       <p>Por gentileza, aguarde nosso contato com a confirmação da abertura de sua conta.</p>
+        </div>
       </section>
     )
   }
 }
-
-export default Confirmation;
+export default Confirm;
+// export default withFirebaseAuth({
+//   firebaseAppAuth,
+// })(Home);
